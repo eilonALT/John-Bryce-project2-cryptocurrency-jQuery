7aa3223718a4c7f2be5c7c90f55edb48b5357fc9
eilon <eilon159123@gmail.com>
2022-01-10 18:33:13 +0200

    all

---
+++ index.js
@@ -0,0 +1,92 @@
+$(function () {
+
+    const loadcoins = async function (data) {
+        let cardsData = await data
+        $("#coins-container").empty()
+
+        let cards = ""
+        for (let i = 0; i < 50; i++) {
+            const element = cardsData[i];
+
+            cards += `
+            <div class="card text-black bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-header">${element.symbol}</div>
+                    <div class="card-body">
+                        <h5 class="card-title">${element.name}</h5>
+                        <div id="${element.id}" class="card-text">
+                            <button class="show-more-button btn btn-outline-primary">show more</button>
+                            <div class="show-more-info"></div>
+                            <button class="show-less-button btn btn-outline-primary">show less</button>
+                        </div>
+                    </div>
+            </div>
+            `
+        }
+        $("#coins-container").append(cards)
+        $(".show-more-info").hide()
+        $(".show-less-button").hide()
+    }
+
+    const loadMoreInfo = function (data, element) {
+        element.empty()
+
+        let moreInfo = ""
+        moreInfo += `
+            <hr>
+            <img src="${data.image.small}" alt="none"><br><br>
+            <p>USD : ${data.market_data.current_price.usd}$</p>
+            <p>EUR : ${data.market_data.current_price.eur}€</p>
+            <p>ILS : ${data.market_data.current_price.ils}₪</p><br>
+        `
+        element.append(moreInfo)
+        element.toggle("slow")
+        element.parent().children("button").toggle("fast")
+    }
+
+
+    $("#show-coins-button").on("click", function () {
+        $.ajax({
+            url: "https://api.coingecko.com/api/v3/coins/list",
+            success: data => {
+                //console.table(data)
+                loadcoins(data)
+            }
+        })
+    })
+
+    $(document).on("click", ".show-more-button", function () {
+        let id = ($(this).parent().attr("id"))
+        $.ajax({
+            url: `https://api.coingecko.com/api/v3/coins/${id}`,
+            success: data => {
+                //console.table(data)
+                loadMoreInfo(data, $(this).parent().children("div"))
+            }
+        })
+    })
+
+    $("#about-button").on("click", function () {
+        $("#coins-container").empty()
+        let about = `
+            <p class ="about">
+                author: eilon alter <br>
+                date: 09/01/2022 <br><br>
+                this website ....
+            </p>
+        `
+        $("#coins-container").append(about)
+    })
+
+    $(document).on("click", ".show-less-button", function () {
+        $(this).parent().children("div").toggle("slow")
+        $(this).parent().children("button").toggle("fast")
+    })
+
+
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".card").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+})
